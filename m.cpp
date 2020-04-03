a19f717e5f4636adbfb8465514d131fcefcf34f5
fanghuixing <fanghuixing@gmail.com>
2020-04-03 10:32:14 +0800

    init

---
+++ m.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <vector>
+
+#include <string>
+ 
+
+using namespace std;
+
+int main()
+{
+    
+   cout<<"hello 回家"<<endl;
+    cout << endl;
+    int i;
+    return 0;
+} 
---
+++ n.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello 知识点  World!");
+    return 0;
+}
